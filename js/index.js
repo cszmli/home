c4837b69c41a77c60065efc95359da476e8dd203
cszmli <z.li@wcw-self-145-109-124-242.wireless.uva.nl>
2019-09-25 11:19:37 +0200

    final

--- js/index.js
+++ js/index.js
@@ -17,27 +17,27 @@ Since: 06/08/2016
 		];
 
   	    $scope.papersUVA = [
-			{'name': 'Dialogue generation: From imitation learning to inverse reinforcement learning',
+			{'name': 'Dialogue generation: From imitation learning to inverse reinforcement learning. ',
 			'journal': 'AAAI 2019',
 			'author': 'Li, Z., Kiseleva, J. & de Rijke, M.',
 			'link': 'https://arxiv.org/abs/1812.03509' 
 			},
-			{'name': 'Optimizing interactive systems with data-driven objectives.',
+			{'name': 'Optimizing interactive systems with data-driven objectives. ',
 			'journal': 'under submission',
 			'author': 'Li, Z., Grotov, A., Kiseleva, J., de Rijke, M. & Oosterhuis, H.',
 			'link': 'https://arxiv.org/abs/1802.06306' 
 			},
-			{'name': 'Diagnosing and optimizing a task-oriented dialogue system with reward shaping',
+			{'name': 'Diagnosing and optimizing a task-oriented dialogue system with reward shaping. ',
 			'journal': 'under submission',
 			'author': 'Li, Z., Kiseleva, J. & de Rijke, M.',
-			'link': 'https://arxiv.org/abs/1812.03509' 
+			'link': 'cszmli.github.io' 
 			},
-			{'name': 'The impact of linkage methods in hierarchical clustering for active learning to rank',
+			{'name': 'The impact of linkage methods in hierarchical clustering for active learning to rank. ',
 			'journal': 'SIGIR 2017',
 			'author': 'Li, Z. & de Rijke, M. (2017). ',
 			'link': 'https://dl.acm.org/citation.cfm?id=3080684' 
 			},
-			{'name': 'Towards learning reward functions from user interactions',
+			{'name': 'Towards learning reward functions from user interactions. ',
 			'journal': 'ICTIR 2017',
 			'author': 'Li, Z., Kiseleva, J., de Rijke, M. & Grotov, A.',
 			'link': 'https://dl.acm.org/citation.cfm?id=3080684' 
@@ -45,15 +45,15 @@ Since: 06/08/2016
 		];
 
 		$scope.papersXiamen = [
-			{'name': 'Solving multidimensional 0-1 knapsack problem with time-free tissue p systems.',
+			{'name': 'Solving multidimensional 0-1 knapsack problem with time-free tissue p systems. ',
 			'journal': 'Journal of Applied Mathematics.',
 			'author': 'Liu, X., Li, Z., Suo, J., Ju, Y., Liu, J. & Zeng, X.'
 			},
-			{'name': 'A uniform solution to integer factorization using time-free spiking neural p system.',
+			{'name': 'A uniform solution to integer factorization using time-free spiking neural p system. ',
 			'journal': 'Neural Computing and Applications',
 			'author': 'Liu, X., Li, Z., Suo, J., Liu, J. & Min, X.'
 			},
-			{'name': 'Implementation of arithmetic operations with time-free spiking neural p systems.',
+			{'name': 'Implementation of arithmetic operations with time-free spiking neural p systems. ',
 			'journal': 'IEEE transactions on nanobioscience',
 			'author': 'Liu, X., Li, Z., Liu, J., Liu, L. & Zeng, X.'
 			},
